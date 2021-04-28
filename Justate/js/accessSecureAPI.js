f0dc9990703cbce6af560a91dce4d18516acc392
Filipp Ruskih <filippruskih@gmail.com>
2021-04-28 18:53:24 +0100

    ndvlsn

---
+++ Justate/js/accessSecureAPI.js
@@ -0,0 +1,51 @@
+function getSecureAPI() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://us-central1-justateapp.cloudfunctions.net/authorizedendpoint');
+
+  // Track the state changes of the request.    
+  xhr.onreadystatechange = function () {
+    var DONE = 4; // readyState 4 means the request is done.        
+    var OK = 200; // status 200 is a successful return.        
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+        console.log(xhr.responseText);
+        response.innerHTML = xhr.responseText;
+      }
+    } else {
+      response.innerHTML = "Unauthorized to view this content";
+      console.log('Error: ' + xhr.status); // An error occurred during the request.        
+    }
+  };
+  // Set the Authorization header    
+  xhr.setRequestHeader('Authorization', 'Bearer ' + getCookie('accessToken'))
+  xhr.send(null);
+}
+
+// W3C Schools
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function logout() {
+  firebase.auth().signOut().then(() => {
+    console.log("Sign out successful");
+    // Reset cookie    
+    document.cookie = "accessToken= ";
+    // Redirect to the home page    
+    window.location.href = "/index.html"
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
+}
---
+++ Justate/js/login.js
@@ -0,0 +1,25 @@
+function login() 
+{
+    let email = document.getElementById('exampleInputEmail1').value    
+    let password = document.getElementById('exampleInputPassword1').value   
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {            
+        // Signed in            
+        var user = userCredential.user;            
+        // If successful redirect to a secure page            
+        
+        document.cookie = "accessToken="+ user.za; 
+        document.cookie = "uid=" + user.uid;  
+        console.log(user);     
+        window.location.href = "/secure.html"            
+    
+        // ...        
+    })        
+    .catch((error) => {            
+        var errorCode = error.code;            
+        var errorMessage = error.message;            
+        console.log(errorCode, errorMessage);        
+    });
+}
+ 
