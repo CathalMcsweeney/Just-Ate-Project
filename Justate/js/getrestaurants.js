bb4d71a568377b875dfa2fefce592f62b78f1a3e
Jakub Kornacki <jakubkornacki358@gmail.com>
2021-05-03 17:56:46 +0200

    fixed getrestaurants

--- Justate/js/getrestaurants.js
+++ Justate/js/getrestaurants.js
@@ -21,23 +21,21 @@ function getrestaurants()
         {
             if (xhr.status === OK) 
             {
-                
+
                 if(xhr.responseText != "Restaurants collection empty")
                 {
                     var data = JSON.parse(xhr.responseText); 
                     var sHTML = "<div class='container'>";
                     for(var i = 0; i < data.length; i++){
-                        // every second restaurant add an extra row
-                        if(i % 2 == 0){
-                            sHTML += "<div class='row'>";
-                        }
+                  
+                
                         // general information
                         sHTML += "<div class='card col-6' style='width: 32rem;'><h5 class='card-header text-center font-weight-bold text-white bg-primary'>" + data[i].name + "</h5>"
                         sHTML += "<div class='card-body font-weight-bold'><div class='row'><div class='col-6'>Cuisine: " + data[i].cuisine + "</div><div class='col-6'>Order Limit: " + data[i].orderlim + "</div></div>";
                         sHTML += "<div class='row'><div class='col-6'>Longitude: " + data[i].longitude + "</div><div class='col-6'>Latitude: " + data[i].latitude + "</div></div>";
                         sHTML += "<div class='row'><div class='col-6'>Weekday Open: " + data[i].weekdayOpen + "</div><div class='col-6'>Weekday Close: " + data[i].weekdayClose + "</div></div>";
                         sHTML += "<div class='row'><div class='col-6'>Weekend Open: " + data[i].weekendOpen + "</div><div class='col-6'>Weekend Close: " + data[i].weekendClose + "</div></div>";
-                       
+
                         // starters
                         sHTML += "<div class='row' id='starters'><h6 class='text-center col-12'>Starters</h6>";
                         for(var j = 0; j < data[i].starters.length; j++){
@@ -48,7 +46,7 @@ function getrestaurants()
                         // mains
                         sHTML += "<div class='row' id='mains'><h6 class='text-center col-12'>Mains</h6>";
                         for(var j = 0; j < data[i].mains.length; j++){
-                            sHTML += "<button class='btn btn-primary text-white text-center font-weight-bold'>" + data[i].mains[j].item + " " + data[i].mains[i].price + " €" + "</button>";
+                            sHTML += "<button class='btn btn-primary text-white text-center font-weight-bold'>" + data[i].mains[j].item + " " + data[i].mains[j].price + " €" + "</button>";
                         }
                         sHTML += "</div>";
 
@@ -60,16 +58,14 @@ function getrestaurants()
                         sHTML += "</div></div></div>";
 
                         // close off the row for with the two restaurants displayed and new row if needed
-                        if(i % 2 == 0 && i != 0){
-                            sHTML += "</div>";
-                        }
+                   
 
                     }
 
                      sHTML += "</div>";
-                
+
                     restaurants.innerHTML = sHTML;
-                    
+
 
                 }           
             } 
@@ -80,4 +76,4 @@ function getrestaurants()
         }
     };
     xhr.send(null);
-}
+} 
