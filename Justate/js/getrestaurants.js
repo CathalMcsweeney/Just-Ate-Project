2296f253c10f213a0ea0907a73e15f80d07323ba
Jakub Kornacki <jakubkornacki358@gmail.com>
2021-05-04 00:00:37 +0200

    updated getrestaurants

--- Justate/js/getrestaurants.js
+++ Justate/js/getrestaurants.js
@@ -25,20 +25,45 @@ function getrestaurants()
                 if(xhr.responseText != "Restaurants collection empty")
                 {
                     var data = JSON.parse(xhr.responseText); 
-
-                    var mHTML = "<section class='page-section bg-light' id='portfolio'><div class='container'><div class='text-center'><h2 class='section-heading text-uppercase'>Restaurants that we have teamed up with.</h2><h3 class='section-subheading text-muted'>Amazing selections from worlds best cuisines.</h3></div><div class='row'>";
+                    // visible icons of the modals -> when clicked displays the inside of the modal
+                    var mHTML = "<section class='page-section bg-light' id='portfolio'>";
+                    mHTML += "<div class='container'>";
+                    mHTML += "<div class='text-center'>";
+                    mHTML += "<h2 class='section-heading text-uppercase'>Restaurants that we have teamed up with.</h2>";
+                    mHTML += "<h3 class='section-subheading text-muted'>Amazing selections from worlds best cuisines.</h3>"
+                    mHTML += "</div><div class='row'>";
                     for(var i = 0; i < data.length; i++){
-                        mHTML += "  <div class='col-lg-4 col-sm-6 mb-4'><div class='portfolio-item'><a class='portfolio-link' data-toggle='modal' href='#portfolioModal"+i+"'><div class='portfolio-hover'><div class='portfolio-hover-content'><i class='fas fa-plus fa-3x'></i></div></div><img class='img-fluid' src='assets/img/restaurants/kfc.png'alt='' /></a><div class='portfolio-caption'><div class='portfolio-caption-heading'>KFC</div><div class='portfolio-caption-subheading text-muted'>American Chicken</div></div></div></div>";
+                        mHTML += "<div class='col-lg-4 col-sm-6 mb-4'>";
+                        mHTML += "<div class='portfolio-item'>";
+                        mHTML += "<a class='portfolio-link' data-toggle='modal' href='#portfolioModal"+i+"'>";
+                        mHTML += "<div class='portfolio-hover'>";
+                        mHTML += "<div class='portfolio-hover-content'><i class='fas fa-plus fa-3x'></i></div></div>";
+                        mHTML += "<img class='img-fluid' src='assets/img/restaurants/kfc.png'alt='' />";
+                        mHTML += "</a><div class='portfolio-caption'>";
+                        mHTML += "<div class='portfolio-caption-heading'><p>" + data[i].name + "</p></div>";
+                        mHTML += "<div style='font-size:100%' class='portfolio-caption-heading'><p>" + data[i].cuisine + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Latitude: " + data[i].latitude + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Longitude: " + data[i].longitude + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Order Limit: " + data[i].orderlim + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Weekday Opening: " + data[i].weekdayOpen + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Weekday Closing: " + data[i].weekdayClose + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Weekend Opening: " + data[i].weekendOpen + "</p></div>";
+                        mHTML += "<div class='portfolio-caption-subheading text-muted'><p>Weekend Closing: " + data[i].weekendClose + "</p></div>";
+                        mHTML += "</div></div></div>";
                     }
                     mHTML += "</div></div></section>";
-                    triggermodals.innerHTML = mHTML;
+                    displayModal.innerHTML = mHTML;
 
-
+                    // inside of the modals containing the menu for the restaurant
                     var sHTML = "";
                     for(var i = 0; i < data.length; i++){
-                        sHTML += "<div class='portfolio-modal modal fade' id='portfolioModal"+i+"' tabindex='-1' role='dialog' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='close-modal' data-dismiss='modal'><img src='assets/img/close-icon.svg' alt='Close modal' /></div><div class='container'><div class='now justify-content-center'><div class='col-lg-8'><div class='modal-body'><h2 class='text-uppercase'>Project Name</h2><p class='item-intro text-muted'>Lorem ipsum dolor sit amet consectetur.</p><p class='item-intro text-muted'>Make a table with a tick selection to be able to tick off and choose items of the menu</p><p>Use this area to describe your project. !</p><ul class='list-inline'><li>Date: January 2020 // Make the total amount of items in the basket from this restaurant here</li><li>Client: Threads // List the items in the basket</li><li>Category: Illustration // Total price for this restaurants basket</li></ul><button class='btn btn-primary' data-dismiss='modal' type='button'><i class='fas fa-times mr-1'></i>Close Menu and Add to Main Basket</button></div></div></div></div></div></div></div>";
+                        sHTML += "<div class='portfolio-modal modal fade' id='portfolioModal"+i+"' tabindex='-1' role='dialog' aria-hidden='true'>";
+                        sHTML += "<div class='modal-dialog'><div class='modal-content'>";
+                        sHTML += "<div class='close-modal' data-dismiss='modal'><img src='assets/img/close-icon.svg' alt='Close modal' /></div>";
+                        sHTML += "<div class='container'><div class='now justify-content-center'><div class='col-lg-16'><div class='modal-body'>";
+                        sHTML += "<button class='btn btn-primary' data-dismiss='modal' type='button'><i class='fas fa-times mr-1'></i>Close Menu and Add to Main Basket</button>";
+                        sHTML += "</div></div></div></div></div></div></div>";
                     }
-                    
                     modals.innerHTML = sHTML;
                 
                 }           
