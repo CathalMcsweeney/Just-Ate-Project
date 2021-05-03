b119bd29be9ad2116ed153b051d9b108b97cdb34
Jakub Kornacki <jakubkornacki358@gmail.com>
2021-05-03 22:05:19 +0200

    started getrestaurant functionality (not finished)

--- Justate/js/getrestaurants.js
+++ Justate/js/getrestaurants.js
@@ -21,52 +21,26 @@ function getrestaurants()
         {
             if (xhr.status === OK) 
             {
-
+                
                 if(xhr.responseText != "Restaurants collection empty")
                 {
                     var data = JSON.parse(xhr.responseText); 
-                    var sHTML = "<div class='container'>";
+
+                    var mHTML = "<section class='page-section bg-light' id='portfolio'><div class='container'><div class='text-center'><h2 class='section-heading text-uppercase'>Restaurants that we have teamed up with.</h2><h3 class='section-subheading text-muted'>Amazing selections from worlds best cuisines.</h3></div><div class='row'>";
                     for(var i = 0; i < data.length; i++){
-                  
-                
-                        // general information
-                        sHTML += "<div class='card col-6' style='width: 32rem;'><h5 class='card-header text-center font-weight-bold text-white bg-primary'>" + data[i].name + "</h5>"
-                        sHTML += "<div class='card-body font-weight-bold'><div class='row'><div class='col-6'>Cuisine: " + data[i].cuisine + "</div><div class='col-6'>Order Limit: " + data[i].orderlim + "</div></div>";
-                        sHTML += "<div class='row'><div class='col-6'>Longitude: " + data[i].longitude + "</div><div class='col-6'>Latitude: " + data[i].latitude + "</div></div>";
-                        sHTML += "<div class='row'><div class='col-6'>Weekday Open: " + data[i].weekdayOpen + "</div><div class='col-6'>Weekday Close: " + data[i].weekdayClose + "</div></div>";
-                        sHTML += "<div class='row'><div class='col-6'>Weekend Open: " + data[i].weekendOpen + "</div><div class='col-6'>Weekend Close: " + data[i].weekendClose + "</div></div>";
-
-                        // starters
-                        sHTML += "<div class='row' id='starters'><h6 class='text-center col-12'>Starters</h6>";
-                        for(var j = 0; j < data[i].starters.length; j++){
-                            sHTML += "<button class='btn btn-primary text-white text-center font-weight-bold'>" + data[i].starters[j].item + " " + data[i].starters[j].price + " €" + "</button>";
-                        }
-                        sHTML += "</div>";
-
-                        // mains
-                        sHTML += "<div class='row' id='mains'><h6 class='text-center col-12'>Mains</h6>";
-                        for(var j = 0; j < data[i].mains.length; j++){
-                            sHTML += "<button class='btn btn-primary text-white text-center font-weight-bold'>" + data[i].mains[j].item + " " + data[i].mains[j].price + " €" + "</button>";
-                        }
-                        sHTML += "</div>";
-
-                        // desserts
-                        sHTML += "<div class='row' id='desserts'><h6 class='text-center col-12'>Desserts</h6>";
-                        for(var j = 0; j < data[i].desserts.length; j++){
-                            sHTML += "<button class='btn btn-primary text-white text-center font-weight-bold'>" + data[i].desserts[j].item + " " + data[i].desserts[j].price + " €" + "</button>";
-                        }
-                        sHTML += "</div></div></div>";
-
-                        // close off the row for with the two restaurants displayed and new row if needed
-                   
-
+                        mHTML += "  <div class='col-lg-4 col-sm-6 mb-4'><div class='portfolio-item'><a class='portfolio-link' data-toggle='modal' href='#portfolioModal"+i+"'><div class='portfolio-hover'><div class='portfolio-hover-content'><i class='fas fa-plus fa-3x'></i></div></div><img class='img-fluid' src='assets/img/restaurants/kfc.png'alt='' /></a><div class='portfolio-caption'><div class='portfolio-caption-heading'>KFC</div><div class='portfolio-caption-subheading text-muted'>American Chicken</div></div></div></div>";
                     }
-
-                     sHTML += "</div>";
-
-                    restaurants.innerHTML = sHTML;
+                    mHTML += "</div></div></section>";
+                    triggermodals.innerHTML = mHTML;
 
 
+                    var sHTML = "";
+                    for(var i = 0; i < data.length; i++){
+                        sHTML += "<div class='portfolio-modal modal fade' id='portfolioModal"+i+"' tabindex='-1' role='dialog' aria-hidden='true'><div class='modal-dialog'><div class='modal-content'><div class='close-modal' data-dismiss='modal'><img src='assets/img/close-icon.svg' alt='Close modal' /></div><div class='container'><div class='now justify-content-center'><div class='col-lg-8'><div class='modal-body'><h2 class='text-uppercase'>Project Name</h2><p class='item-intro text-muted'>Lorem ipsum dolor sit amet consectetur.</p><p class='item-intro text-muted'>Make a table with a tick selection to be able to tick off and choose items of the menu</p><p>Use this area to describe your project. !</p><ul class='list-inline'><li>Date: January 2020 // Make the total amount of items in the basket from this restaurant here</li><li>Client: Threads // List the items in the basket</li><li>Category: Illustration // Total price for this restaurants basket</li></ul><button class='btn btn-primary' data-dismiss='modal' type='button'><i class='fas fa-times mr-1'></i>Close Menu and Add to Main Basket</button></div></div></div></div></div></div></div>";
+                    }
+                    
+                    modals.innerHTML = sHTML;
+                
                 }           
             } 
             else 
@@ -76,4 +50,4 @@ function getrestaurants()
         }
     };
     xhr.send(null);
-} 
+}
