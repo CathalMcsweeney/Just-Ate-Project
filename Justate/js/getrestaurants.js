2a697dbb5cb6179fe711e5214e059111a5697562
Filipp Ruskih <filippruskih@gmail.com>
2021-05-06 09:29:24 +0100

    delete mnowww

--- Justate/js/getrestaurants.js
+++ Justate/js/getrestaurants.js
@@ -66,24 +66,16 @@ function getrestaurants()
                             var restaurant = {"name":data[i].name};
                             var obj = Object.assign({},restaurant,data[i].starters[j]);
                             var dataToCart = JSON.stringify(obj);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onmouseup='buttonReleased(this);' onmousedown='buttonPressed(this);'  onclick='addToList("+dataToCart+"); toggleButtonClass(this);' > Add to Cart </button> </p>";
-=======
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");' > Add to Cart </button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
-=======
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");' > Add to Cart </button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
-=======
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");' > Add to Cart </button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
+                            var restUID = JSON.stringify(data[i].uid);
+                            sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");' > Add to Cart </button> </p>";
                             sHTML += " </div></div>";
-
                         }
                         sHTML += "</div>";
 
@@ -98,22 +90,19 @@ function getrestaurants()
                             var restaurant = {"name":data[i].name};
                             var obj = Object.assign({},restaurant,data[i].mains[j]);
                             var dataToCart = JSON.stringify(obj);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
                             sHTML += "<p> <button class='btn btn-primary btn-sm' type='button' onmouseup='buttonReleased(this);' onmousedown='buttonPressed(this);'  onclick='addToList("+dataToCart+"); toggleButtonClass(this);' > Add to Cart </button> </p>";
-=======
+                            
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p> <button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+ restUID+");' > Add to Cart </button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
-=======
+                            
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p> <button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+ restUID+");' > Add to Cart </button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
-=======
+                            
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p> <button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+ restUID+");' > Add to Cart </button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
+                            
+                            var restUID = JSON.stringify(data[i].uid);
+                            sHTML += "<p> <button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+ restUID+");' > Add to Cart </button> </p>";
                             sHTML += " </div></div>";
                         }
                         sHTML += "</div>";
@@ -129,22 +118,19 @@ function getrestaurants()
                             var restaurant = {"name":data[i].name};
                             var obj = Object.assign({},restaurant,data[i].desserts[j]);
                             var dataToCart = JSON.stringify(obj);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onmouseup='buttonReleased(this);' onmousedown='buttonPressed(this);' onclick='addToList("+dataToCart+");'>Add to Cart</button> </p>";
-=======
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");'>Add to Cart</button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
-=======
+
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");'>Add to Cart</button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
-=======
+
                             var restUID = JSON.stringify(data[i].uid);
                             sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");'>Add to Cart</button> </p>";
->>>>>>> 14237888875262f6362d53bd10bd1a2a303ccb60
+
+                            var restUID = JSON.stringify(data[i].uid);
+                            sHTML += "<p><button class='btn btn-primary btn-sm' type='button' onclick='checkNumRest("+dataToCart+","+restUID+");'>Add to Cart</button> </p>";
                             sHTML += " </div></div>";
                         }
                         sHTML += "</div>";
